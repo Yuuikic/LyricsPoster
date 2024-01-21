e32bfff321cf8c350ee76c9d8f2351164d8118ac
桜庭夜 <1149720516@qq.com>
2024-01-21 12:53:44 +0800

    A New Version

---
+++ src/SongResult.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { ref } from 'vue';
+import { lyrics } from './lyrics.js';
+
+defineProps(['title', 'artist', 'albumCover', 'id']);
+const isClicked = ref(false);
+
+
+const handleClick = (id, title, artist) => {
+    lyrics.getLyrics(id, isClicked);
+    lyrics.title = title;
+    lyrics.artist = artist;
+};
+
+</script>
+
+<template>
+    <div class="songResult" @click="lyrics.albumCover = albumCover, handleClick(id, title, artist)"
+        :class="{ clicked: isClicked }">
+        <div class="albumCover" :style="{ backgroundImage: `url('${albumCover}?param=500y500')` }"></div>
+        <div class="songTitle w-3/4">
+            <p>{{ title }}</p>
+            <p>{{ artist }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.clicked {
+    background-color: rgba(175, 175, 255, 0.449);
+    background-size: 200%;
+    animation: clickedAnime 1s linear infinite alternate;
+    transition: all 0.5s linear;
+}
+
+@keyframes clickedAnime {
+    to {
+        background-color: rgba(113, 137, 255, 0.449);
+    }
+}
+
+.albumCover {
+    min-width: 48px;
+    height: 48px;
+    margin: 8px;
+    border-radius: 7px;
+    background-position: center;
+    background-size: cover;
+}
+
+.songTitle {
+    margin: 8px 8px 8px 0px;
+}
+
+.songTitle p:first-child {
+    /* font-size: 1.2rem; */
+    max-height: 2rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    scrollbar-width: none;
+}
+
+.songTitle p:first-child::-webkit-scrollbar {
+    display: none;
+}
+
+.songTitle p:last-child {
+    font-size: 0.8rem;
+    color: rgb(105, 101, 101);
+    overflow: scroll;
+    scrollbar-width: none;
+}
+
+.songTitle p:last-child::-webkit-scrollbar {
+    display: none;
+}
+
+.songResult:hover {
+    background-color: rgba(175, 175, 255, 0.449);
+    cursor: pointer;
+}
+
+.songResult {
+    width: 100%;
+    border: rgb(248, 248, 248) 1.2px solid;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    transition: all 200ms linear;
+}
+</style>
---
+++ src/lyrics.js
@@ -0,0 +1,81 @@
+import { reactive } from 'vue';
+import anime from 'animejs';
+
+export const lyrics = reactive({
+    // originLyricsArray: '',
+    lyricsArray: '',
+    lyricsArrayWithSelectedStatus: [],
+    tlyricsArray: '',
+    tlyricsArrayWithSelectedStatus: [],
+    showedLyrics: '',
+    title: '',
+    artist: '',
+    albumCover: '',
+    uploadedCover: '',
+    albumMainColor: '',
+    albumColorPlatte: '',
+    searchAPIUrl: "https://api.taions.nonon.moe",
+    hasGotLyrics: '',
+    hasShownLyrics: '',
+    changeSelectedStatus(isSelected) {
+        isSelected.value = !isSelected.value;
+    },
+    changeLyricsStatus(hasGotLyrics) {
+        hasGotLyrics.value = !hasGotLyrics.value;
+    },
+    async getLyrics(id, isClicked) {
+        let lyricsURL = `${this.searchAPIUrl}/lyric?id=${id}`;
+        isClicked.value = !isClicked.value; // 更新传入的 ref
+
+        fetch(lyricsURL, {
+            method: "GET",
+            credentials: 'include'
+        }).then(
+            (response) => {
+                isClicked.value = !isClicked.value; // 更新传入的 ref
+                return response.json()
+            }
+        ).then((lyricInfo) => {
+            // 定义获取歌词和歌词翻译的方法
+            const getLyricsOnly = () => {
+                let lyrics = lyricInfo.lrc.lyric;
+                const lyricLines = lyrics.match(/\[\d+:\d+\.\d+\]([^\n]+)/g);
+                this.lyricsArray = lyricLines.map(line => {
+                    const lyricContent = line.replace(/\[\d+:\d+\.\d+\]/, '').trim();
+                    return lyricContent;
+                });
+                for (let i = 0; i < this.lyricsArray.length; i++) {
+                    this.lyricsArrayWithSelectedStatus[i] = { text: this.lyricsArray[i], isSelected: false, index: 'n' + i }
+                }
+            }
+
+            const getTLyricsOnly = () => {
+                let tlyrics = lyricInfo.tlyric.lyric;
+                const tlyricLines = tlyrics.match(/\[\d+:\d+\.\d+\]([^\n]+)/g);
+                this.tlyricsArray = tlyricLines.map(line => {
+                    const tlyricContent = line.replace(/\[\d+:\d+\.\d+\]/, '').trim();
+                    return tlyricContent;
+                });
+                for (let i = 0; i < this.tlyricsArray.length; i++) {
+                    this.tlyricsArrayWithSelectedStatus[i] = { text: this.tlyricsArray[i], isSelected: false, index: 't' + i }
+                }
+            }
+
+            // 通过歌词JSON信息判断含有的歌词类型，并处理歌词
+            if (lyricInfo.lrc.lyric === "") {
+                alert("暂无歌词🫠")
+            } else if (lyricInfo.qfy == false && lyricInfo.tlyric && lyricInfo.tlyric.lyric === "") {
+                getLyricsOnly();
+                // console.log(this.lyricsArray);
+            } else if (lyricInfo.qfy == false && lyricInfo.tlyric && lyricInfo.tlyric.lyric != "") {
+                getLyricsOnly();
+                getTLyricsOnly();
+                // console.log(this.lyricsArray);
+                // console.log(this.tlyricsArray);
+            } else {
+                getLyricsOnly();
+                // console.log(this.lyricsArray);
+            }
+        });
+    }
+});
